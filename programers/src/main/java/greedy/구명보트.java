7c3ebe522f18bd9e18eab743fde27f26799780e5
김형익 <kiomnd2@naver.com>
2024-12-31 16:54:13 +0900

    구명보트

---
+++ programers/src/main/java/greedy/구명보트.java
@@ -0,0 +1,26 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class 구명보트 {
+
+    public int solution(int[] people, int limit) {
+        int answer =0;
+
+        Integer[] pepObj = Arrays.stream(people).boxed().toArray(Integer[]::new);
+        Arrays.sort(pepObj, Comparator.reverseOrder());
+
+        int rightCnt =0;
+        for (int i=0 ; i<pepObj.length - rightCnt ; i++) {
+            int p1 = pepObj[i];
+            if (p1 == -1) continue;
+
+            if (p1 + pepObj[people.length -1 - rightCnt] <= limit) {
+                rightCnt++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
