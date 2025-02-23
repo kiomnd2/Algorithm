c8a657018fbd921bc6953d1d546b0241d4976188
kimHI <kiomnd2@naver.com>
2025-02-23 23:06:08 +0900

    [level 1] Title: 둘만의 암호, Time: 21.76 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,20 +1,30 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            for(int j = 0; j < index; j++) {
-                a = (char)(a + 1);
-                if(skip.contains(a+"")) j--;
-                if((int)a > 122) {
-                    a -= 26;
-                    if(skip.contains(a+"")) j--;
+        for (int i=0 ; i<s.length() ; i++) {
+            char c = s.charAt(i);
+            int skipIdx = index;
+            int cIdx = ((int)c) + skipIdx;
+
+            for (int j=0 ; j<skip.length() ; j++) {
+                char c1 = skip.charAt(j);
+                // 스킵할 인덱스가 사이에 있으면
+                if (c <= c1 && cIdx >= c1) {
+                    skipIdx++; // 스킵할 인덱스 +1
                 }
             }
-            answer += a;
+
+
+            int i1 = (c + skipIdx);
+            if(i1 > 122) { // z 가 넘어가면
+                int diff = i1 % 122 -1;
+                i1 = 97 + diff;
+            }
+
+            sb.append((char)i1);
         }
 
-        return answer;
+        return sb.toString();
     }
 }
