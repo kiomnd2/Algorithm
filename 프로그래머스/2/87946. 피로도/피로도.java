72754d4fae16ef30ede1e496937a2ce9ae4e3eb9
kimHI <kiomnd2@naver.com>
2025-09-03 22:34:06 +0900

    [level 2] Title: 피로도, Time: 3.28 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(int k, int[][] dungeons) {
+
+        for (int i = 0 ; i < dungeons.length ; i++) {
+            int[] dungeon = dungeons[i];
+            int minFigure = dungeon[0];
+            int consume = dungeon[1];
+            boolean[] visit = new boolean[dungeons.length];
+
+            if (minFigure <= k) {
+                visit[i] = true;
+                dfs(dungeons, visit, 1, k-consume);
+            }
+
+        }
+        return maxCount;
+    }
+
+    int maxCount = Integer.MIN_VALUE;
+    private void dfs(int[][] dungeons, boolean[] visit, int countVisit, int leftFigure) {
+
+        for (int i = 0 ; i < dungeons.length ; i++) {
+            int[] dungeon = dungeons[i];
+            int minFigure = dungeon[0];
+            int consume = dungeon[1];
+
+            if (!visit[i] && leftFigure >= minFigure) {
+                visit[i] = true;
+                dfs(dungeons, visit, countVisit+1, leftFigure-consume);
+                visit[i] =false;
+            }
+        }
+        maxCount = Math.max(maxCount, countVisit);
+    }
+}
